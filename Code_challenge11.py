9bd9a724e866397b42c9fb9b5325662b4020adf8
neilpaghubasan4-stack <neilpaghubasan4@gmail.com>
2025-10-27 13:44:31 +0800

    Create Code_challenge11.py

---
+++ Code_challenge11.py
@@ -0,0 +1,8 @@
+for i in range(1,11,1):
+  for x in range(10,i,-1):
+    print(" ",end=" ")
+  for y in range(1,i,1):
+    print("*",end=" ")
+  for z in range(1,i,1):
+    print("*",end=" ")
+  print()
