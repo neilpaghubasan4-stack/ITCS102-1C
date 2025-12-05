a89b3655b720e380cf80e5d522420c1ebf0cbb13
neilpaghubasan4-stack <neilpaghubasan4@gmail.com>
2025-12-05 19:33:26 +0800

    Update Final Project.py

--- Final Project.py
+++ Final Project.py
@@ -63,3 +63,78 @@ while True:
             else:
                 print("Please type Yes or No only.")
                        # Loop until user enters 'yes' or 'no'
+
+    elif choice == 'c':
+        print("\n\t\tOperator & Expression\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nOPERATORS & EXPRESSIONS\nContent:\n\nOperators allow you to perform actions on values.\n\nType of operators:\n\n\t1. Arithmetic: +, -, *, /, %, //, **\n\t2. Comparison: ==, !=, >, <, >=, <=\n\t3. Logical: and, or, not\n\nExample:\n\n\ta = 10\n\n\tb = 5\n\n\tresult = a + b\n\nExpressions combine values and operators to produce a result.\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no'   
+
+    elif choice == 'd':
+        print("\n\t\tConditional Statements (if/else)\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nCONDITIONAL STATEMENTS (IF/ELSE)\nContent:\n\nConditionals allow programs to make decisions.\n\nExample:\n\n\tage = 18\n\n\tif age >= 18:\n\t\tprint("You are an adult.)\n\telse:\n\t\tprint("You are a minor.)\n\nThe program chooses a path depending on whether conditions are true or false.\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no'       
+
+    elif choice == 'e':
+        print("\n\t\tLoops (for/while)\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nLOOPS (FOR/WHILE)\nContent:\n\nLoops repeat code multiple times.\n\nFor loop: repeats over a sequence.\n\n\tfor i in range(5):\n\t\tprint(i)\n\nWhile loop: repeats while a condition is true.\n\n\tcount = 1\n\twhile count <= 5:\n\t\tprint(count)\n\t\tcount += 1\n\nLoops help automate repetitive tasks.\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no'     
+    
+    elif choice == 'f':
+        print("\n\t\tFunctions\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nFUNCTIONS\nContent:\n\nFunctions are reusable block of code that perform a specific task.\n\nExample:\n\n\tdef greet(name)\n\t\tprint("Hello,", name)\n\n\tgreet("Neil").\n\nFunction helps organize code and avoid repetition.\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no' 
+
+    elif choice == 'g':
+        print("\n\t\tLists, Tuples, and Dictionaries\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nLISTS, TUPLES, & DICTIONARIES\nContent:\n\nThese are basic Python data structures.\n\nList - changeable, ordered collection.\n\n\tfruits = ["apple", "banana", "mango"]\n\nTuple - ordered but unchangeable.\n\n\tpoint = (10,20)\n\nDictionary - stores key-value pairs.\n\n\tstudent = {"name": "Neil", "age: 18}\n\nEach structure has a unique purpose in organizing data.\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no' 
