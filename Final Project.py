d223640bea07abb47820182383362ae0d020f744
neilpaghubasan4-stack <neilpaghubasan4@gmail.com>
2025-12-05 09:15:21 +0800

    Create Final Project.py

---
+++ Final Project.py
@@ -0,0 +1,65 @@
+username = input("Hi! Welcome to my program. What is your name? ")
+
+# Keep asking yes/no until valid input
+while True:
+    yn = input(f"Hi {username}! Would you like to start the program? (Yes/No) ").lower().strip()
+    
+    if yn == 'yes':
+        break  # Valid input, exit this loop and go to menu
+    elif yn == 'no':
+        print("Goodbye and Thank you.")
+        exit()  # End program
+    else:
+        print("Please type Yes or No only.")
+
+
+
+print("\n\t\tWELCOME TO NMPy PROGRAM!")
+
+while True:
+    print("\n============================================================================================\n")
+    print("\t\tNMPy PROGRAM CONTENTS:\n")
+    print("\tA - Introduction to Programming")
+    print("\tB - Data Types & Variables")
+    print("\tC - Operators & Expressions")
+    print("\tD - Conditional Statements (if/else)")
+    print("\tE - Loops (for/while)")
+    print("\tF - Functions")
+    print("\tG - Lists, Tuples, and Dictionaries")
+    print("\tH - Basic Input/Output")
+    print("\tI - Mini Coding Quiz")
+    print("\tJ - Exit Program\n")
+ 
+    choice = input("Select from the following options: ").lower()
+
+    if choice == 'a':
+        print("\n\t\tIntroduction to Programming\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+        
+            if dec == 'yes':
+                print("\n============================================================================================\n\nINTRODUCTION TO PROGRAMMING\nContent:\n\nProgrammings is the process of giving instructions to a computer so it can perform tasks. \nThis particular programming language (Python) acts as a way for humans to communicate with machines.\n\nKey Points:\n\n\t1. Computer follows instructions step-by-step.\n\t2. Programming helps automate tasks, solve problems, and build apps.\n\t3. Python is popular because it's simple, readable andpowerful.\n\n============================================================================================\n")
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no'
+    
+        continue  # go back to the main menu
+
+    elif choice == 'b':
+        print("\n\t\tData Types & Variables\n")
+
+        while True:
+            dec = input("Proceed? (Yes/No): ").lower().strip()
+
+            if dec == 'yes':
+                print('\n============================================================================================\n\nDATA TYPES & VARIABLES\nContent:\n\nA variable stores a value so it can be used later.\nA data type tells the computer what kind of value is stored.\n\nCommon Python Data Types:\n\n1. int - whole numbers (e.g., 5, 20)\n2. float - decimal numbers (e.g., 3.14)\n3. str - text or characters (e.g., "Hello")\n4. bool - True or False values\n\nExample:\n\n\tage = 18\n\n\tpi = 3.14\n\n\tname = "Neil"\n\n\tis_student = True\n\n============================================================================================')
+                break  # exit the dec loop and return to menu
+            elif dec == 'no':
+                break  # exit the dec loop and return to menu
+            else:
+                print("Please type Yes or No only.")
+                       # Loop until user enters 'yes' or 'no'
