34ec9f14a35b45503706fe3a4ac96d9c0638056c
neilpaghubasan4-stack <neilpaghubasan4@gmail.com>
2026-02-13 13:42:24 +0800

    Update student profile details and background color

--- PAGHUBASAN_HO2.py
+++ PAGHUBASAN_HO2.py
@@ -4,23 +4,23 @@ root = tk.Tk()
 root.title("Student Profile")
 root.geometry("600x600")
 root.resizable(False, True)
-root.configure(bg="#f7b6c8")
+root.configure(bg="white")
+
+tk.Label(root, text="Student Profile", font=("Arial", 24, "bold"), bg="white").pack(pady=20)
+
+tk.Label(root, text="Full Name: Neil M. Paghubasan.", font=("Arial", 14), bg="white").pack(anchor="w", padx=40, pady=5)
+tk.Label(root, text="Age: 18 years old", font=("Arial", 14), bg="white").pack(anchor="w", padx=40, pady=5)
+tk.Label(root, text="Course and Section: BSIT - 1C", font=("Arial", 14), bg="white").pack(anchor="w", padx=40, pady=5)
+tk.Label(root, text="Birthday: September 1, 2007", font=("Arial", 14), bg="white").pack(anchor="w", padx=40, pady=5)
+
+tk.Label(root, text="Personal Motto:", font=("Arial", 14, "bold"), bg="white").pack(anchor="w", padx=40, pady=(20, 5))
 
 tk.Label(
     root,
-    text="Student Profile",
-    font=("Arial", 24, "bold"),
-    bg="#f7b6c8"
-).pack(pady=20)
+    text="If you do something,\nYou'll accomplish a thing.",
+    font=("Arial", 13, "italic"),
+    bg="white",
+    justify="center"
+).pack(anchor="w", padx=60)
 
-tk.Label(root, text="Full Name: Angelica Mae V. Borela", font=("Arial", 14), bg="#f7b6c8").pack(anchor="w", padx=40, pady=5)
-tk.Label(root, text="Age: 23 years old", font=("Arial", 14), bg="#f7b6c8").pack(anchor="w", padx=40, pady=5)
-tk.Label(root, text="Course and Section: BSIT - 3A", font=("Arial", 14), bg="#f7b6c8").pack(anchor="w", padx=40, pady=5)
-tk.Label(root, text="Birthday: February 14, 2002", font=("Arial", 14), bg="#f7b6c8").pack(anchor="w", padx=40, pady=5)
-
-tk.Label(
-    root,
-    text="Personal Motto:",
-    font=("Arial", 14, "bold"),
-    bg="#f7b6c8"
-).pack(anchor="w", padx=40, pady=(
+root.mainloop()
